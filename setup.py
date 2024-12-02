3e8311cbf08c7d48bde931c7114e30181b2517e5
mattlang108 <yvy7zh@virginia.edu>
2024-12-02 14:36:35 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MonteCarlo',
+    version='1.0',
+    description='MonteCarlo Project 1',
+    url='https://github.com/mattlang108/MonteCarlo',
+    author='Matt Lang',
+    author_email='yvy7zh@virginia.edu',
+    license='MIT',
+    packages=find_packages()
+)
